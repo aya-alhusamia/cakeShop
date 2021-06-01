ba2f417a4b0dc696af299657fe12054014024a83
aya-alhusamia <alhusamiaya70@gmail.com>
2021-06-01 15:18:26 +0300

    3 cake

---
+++ src/products.js
@@ -0,0 +1,22 @@
+import img1 from "./Pic/pic1.jpg"
+import img2 from "./Pic/pic2.jpg"
+import img3 from "./Pic/pic3.jpg"
+const products=[
+    {
+        name:"Pound Cake",
+        price:10,
+        image:img1
+
+    },
+    {
+        name :"Biscuit Cake",
+        price  :9,
+        image:img2
+    }
+    ,{
+        name :"Baked Flourless Cake",
+        price  :8,
+        image:img3
+    }
+]
+export default products
